7743eef994a720b9f625f42e4be2ccdbda37137a
elliot <ecouvignou@gmail.com>
2020-05-13 12:38:39 -0700

    AWS multipalyer works on packaged, duct tape method

--- Source/Project4/Project4.Build.cs
+++ Source/Project4/Project4.Build.cs
@@ -6,8 +6,10 @@ public class Project4 : ModuleRules
 {
 	public Project4(ReadOnlyTargetRules Target) : base(Target)
 	{
+		bEnableExceptions = true;
+
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameLiftServerSDK" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameLiftServerSDK", "GameLiftClientSDK" });
 	}
 }
