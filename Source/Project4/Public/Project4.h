702154e2c1ad976c984183aabff04edf33aa9f12
elliot <ecouvignou@gmail.com>
2020-05-13 15:51:16 -0700

    CameraMovement on multipalyer

--- Source/Project4/Public/Project4.h
+++ Source/Project4/Public/Project4.h
@@ -3,3 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+
+#define ISDEDICATED (GEngine->GetNetMode(GetWorld()) == NM_DedicatedServer)
+#define ISLISTEN (GEngine->GetNetMode(GetWorld()) == NM_ListenServer)
+#define ISSTANDALONE (GEngine->GetNetMode(GetWorld()) == NM_Standalone)
+#define ISCLIENT (GEngine->GetNetMode(GetWorld()) == NM_Client)
--- Source/Project4/Public/Project4Character.h
+++ Source/Project4/Public/Project4Character.h
@@ -6,33 +6,44 @@
 #include "GameFramework/Character.h"
 #include "Project4Character.generated.h"
 
-UCLASS(config=Game)
+UCLASS(config = Game)
 class AProject4Character : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
+		/** Camera boom positioning the camera behind the character */
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
 
 	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
+		class UCameraComponent* FollowCamera;
 public:
 	AProject4Character();
 
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseTurnRate;
 
 	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseLookUpRate;
+
+	/* next 3 are variables handling delta, min/max camera zoom values */
+	UPROPERTY(EditAnywhere, Category = Camera)
+		float CameraZoomGranularity;
+
+	UPROPERTY(EditAnywhere, Category = Camera)
+		float CameraZoomMin;
+
+	UPROPERTY(EditAnywhere, Category = Camera)
+		float CameraZoomMax;
+
+	UPROPERTY(EditAnywhere, Category = Camera)
+		float CameraSensitivity;
 
 protected:
 
-	/** Resets HMD orientation in VR. */
-	void OnResetVR();
 
 	/** Called for forwards/backward input */
 	void MoveForward(float Value);
@@ -40,30 +51,42 @@ protected:
 	/** Called for side to side input */
 	void MoveRight(float Value);
 
-	/** 
-	 * Called via input to turn at a given rate. 
+	/**
+	 * Called via input to turn at a given rate.
 	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
 	 */
 	void TurnAtRate(float Rate);
 
-	/**
-	 * Called via input to turn look up/down at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
+	void CameraZoom(float Value);
 
-	/** Handler for when a touch input begins. */
-	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
+	// Binds to on left click pressed
+	void AddInputToCameraRotation();
+	void StartMovePlayerCamera();
+	void StopMovePlayerCamera();
 
-	/** Handler for when a touch input stops. */
-	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
+	// binds to right click pressed
+	void SetPlayerRotationToCamera();
+	void StartPlayerRotationToCamera();
+	void StopPlayerRotationToCamera();
+
+
+private:
+	// private variables until i get comfortable with UPROPERTY
+	class UInputComponent* InputComponent;
+
+	bool doInputRotateCamera;
+	bool doRotatePlayerAndCamera;
+
 
 protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	// End of APawn interface
 
+
 public:
+	virtual void Tick(float DeltaTime) override;
+
 	/** Returns CameraBoom subobject **/
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
