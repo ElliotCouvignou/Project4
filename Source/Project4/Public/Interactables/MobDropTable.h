252582f8110216179122ea145167759834cb01b3
ElliotCouvignou <ecouvignou@gmail.com>
2020-09-30 16:55:06 -0700

    Mid Drop-table implementatoin, switching to checkout nick's branch

---
+++ Source/Project4/Public/Interactables/MobDropTable.h
@@ -0,0 +1,17 @@
+// Project4 Copyright (Elliot Couvignou) Dont steal this mayne :(
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MobDropTable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT4_API UMobDropTable : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
